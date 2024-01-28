35db3e71a4c38911c9a07295ffcf50c04ad1541a
sheikh-20 <sheikhzs1032@gmail.com>
2024-01-28 10:46:01 +0530

    Account UI screen integrated

---
+++ app/src/main/java/com/bitvolper/yogazzz/presentation/home/history/HistoryScreen.kt
@@ -0,0 +1,36 @@
+package com.bitvolper.yogazzz.presentation.home.history
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.bitvolper.yogazzz.presentation.theme.YogaAppTheme
+
+@Composable
+fun HistoryScreen(modifier: Modifier = Modifier, paddingValues: PaddingValues = PaddingValues()) {
+    Column(modifier = modifier.fillMaxSize().wrapContentSize(align = Alignment.Center)) {
+        Text(text = "History Screen")
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun HistoryLightThemePreview() {
+    YogaAppTheme(darkTheme = false) {
+        HistoryScreen()
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun HistoryDarkThemePreview() {
+   YogaAppTheme(darkTheme = true) {
+       HistoryScreen()
+   }
+}
