d4223a3c3e629e8b1e285eb8c9dec8f15ec32a39
sheikh-20 <sheikhzs1032@gmail.com>
2024-02-17 17:41:07 +0530

    Home screen API integrated

--- app/src/main/java/com/bitvolper/yogazzz/presentation/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/bitvolper/yogazzz/presentation/viewmodel/HomeViewModel.kt
@@ -1,19 +1,25 @@
 package com.bitvolper.yogazzz.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.bitvolper.yogazzz.domain.model.UserData
+import com.bitvolper.yogazzz.domain.model.YogaCategoryWithRecommendation
+import com.bitvolper.yogazzz.domain.model.YogaRecommendation
 import com.bitvolper.yogazzz.domain.usecase.HomeUseCase
+import com.bitvolper.yogazzz.utility.Resource
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import timber.log.Timber
 import java.io.IOException
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor(val homeUseCase: HomeUseCase): ViewModel() {
+class HomeViewModel @Inject constructor(private val homeUseCase: HomeUseCase): ViewModel() {
 
     private companion object {
         const val TAG = "HomeViewModel"
@@ -24,6 +30,12 @@ class HomeViewModel @Inject constructor(val homeUseCase: HomeUseCase): ViewModel
     private var _profileInfoUiState = MutableStateFlow<UserData>(UserData(userId = "", userName = "", profilePictureUrl = "", email = ""))
     val profileInfoUiState: StateFlow<UserData> = _profileInfoUiState
 
+
+    private var _homeUIState = MutableStateFlow<Resource<YogaCategoryWithRecommendation>>(Resource.Loading)
+    val homeUIState: StateFlow<Resource<YogaCategoryWithRecommendation>> get() = _homeUIState
+
+    private var _recommendationUIState = MutableStateFlow<Resource<YogaRecommendation>>(Resource.Loading)
+    val recommendationUIState: StateFlow<Resource<YogaRecommendation>> get() = _recommendationUIState
 
     private fun getSignedInUser() {
         auth.currentUser?.apply {
@@ -36,9 +48,23 @@ class HomeViewModel @Inject constructor(val homeUseCase: HomeUseCase): ViewModel
         }
     }
 
-    fun getYogaCategory() {
+    fun getHomeContent() = viewModelScope.launch {
         try {
-            homeUseCase.getYogaCategory()
+            Timber.tag(TAG).d("View model called")
+            homeUseCase.getYogaCategoryWithRecommendation().collectLatest {
+                _homeUIState.value = it
+            }
+        } catch (exception: IOException) {
+            Timber.tag(TAG).e(exception)
+        }
+    }
+
+    fun getYogaRecommendation() = viewModelScope.launch {
+        try {
+            Timber.tag(TAG).d("View model called")
+            homeUseCase.getRecommendation().collectLatest {
+                _recommendationUIState.value = it
+            }
         } catch (exception: IOException) {
             Timber.tag(TAG).e(exception)
         }
@@ -46,6 +72,6 @@ class HomeViewModel @Inject constructor(val homeUseCase: HomeUseCase): ViewModel
 
     init {
         getSignedInUser()
-//        getYogaCategory()/**/
+        getHomeContent()
     }
 }
