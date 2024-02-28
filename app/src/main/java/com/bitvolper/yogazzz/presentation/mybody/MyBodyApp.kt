0b5ce7173eae38d5a8888456484de44159e277c7
sheikh-20 <sheikhzs1032@gmail.com>
2024-02-28 16:34:05 +0530

    Account info UI screen updated

--- app/src/main/java/com/bitvolper/yogazzz/presentation/mybody/MyBodyApp.kt
+++ app/src/main/java/com/bitvolper/yogazzz/presentation/mybody/MyBodyApp.kt
@@ -1,27 +1,209 @@
 package com.bitvolper.yogazzz.presentation.mybody
 
 import android.app.Activity
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Divider
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ArrowBack
+import androidx.compose.material3.BottomSheetDefaults
+import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableDoubleStateOf
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.bitvolper.yogazzz.R
+import com.bitvolper.yogazzz.presentation.viewmodel.AccountViewModel
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MyBodyApp(modifier: Modifier = Modifier) {
+fun MyBodyApp(modifier: Modifier = Modifier, accountViewModel: AccountViewModel = hiltViewModel()) {
+
+    val accountInfoUIState by accountViewModel.accountInfoUIState.collectAsState()
+
+    var showBottomSheet by remember {
+        mutableStateOf<BottomSheet>(BottomSheet.Default)
+    }
+
+    val state = rememberDatePickerState()
+    var openDialog by remember { mutableStateOf(false) }
+
+    if (openDialog) {
+        DatePickerDialog(
+            onDismissRequest = {
+                openDialog = false
+            },
+            confirmButton = {
+                TextButton(
+                    onClick = {
+                        accountViewModel.updateBirthdayDate(state.selectedDateMillis ?: return@TextButton)
+                        openDialog = false
+                    }
+                ) {
+                    Text("OK")
+                }
+            },
+            dismissButton = {
+                TextButton(
+                    onClick = {
+                        openDialog = false
+                    }
+                ) {
+                    Text("CANCEL")
+                }
+            }
+        ) {
+            DatePicker(
+                state = state
+            )
+        }
+    }
+
+
+
+    when (showBottomSheet) {
+        BottomSheet.Default -> {  }
+        BottomSheet.Height -> {
+            BottomSheet(
+                onDismiss = { showBottomSheet = BottomSheet.Default   },
+                onNegativeClick = { showBottomSheet = BottomSheet.Default },
+                onPositiveClick = {
+                    accountViewModel.updateHeight(it.toString().toInt())
+                    accountViewModel.updateUserProfile()
+                    showBottomSheet = BottomSheet.Default
+                },
+                contentSheet = {
+                        onNegativeClick, onPositiveClick ->
+
+                    BottomSheetHeightContent(
+                        onNegativeClick = onNegativeClick,
+                        onPositiveClick = onPositiveClick
+                    )
+                }
+            )
+        }
+        BottomSheet.CurrentWeight -> {
+            BottomSheet(
+                onDismiss = { showBottomSheet = BottomSheet.Default   },
+                onNegativeClick = { showBottomSheet = BottomSheet.Default },
+                onPositiveClick = {
+                    accountViewModel.updateCurrentWeight(it.toString().toDouble())
+                    accountViewModel.updateUserProfile()
+                    showBottomSheet = BottomSheet.Default
+                },
+                contentSheet = {
+                        onNegativeClick, onPositiveClick ->
+
+                    BottomSheetCurrentWeightContent(
+                        onNegativeClick = onNegativeClick,
+                        onPositiveClick = onPositiveClick
+                    )
+                }
+            )
+        }
+        BottomSheet.TargetWeight -> {
+            BottomSheet(
+                onDismiss = { showBottomSheet = BottomSheet.Default   },
+                onNegativeClick = { showBottomSheet = BottomSheet.Default },
+                onPositiveClick = {
+                    accountViewModel.updateTargetWeight(it.toString().toDouble())
+                    accountViewModel.updateUserProfile()
+                    showBottomSheet = BottomSheet.Default
+                },
+                contentSheet = {
+                        onNegativeClick, onPositiveClick ->
+
+                    BottomSheetTargetWeightContent(
+                        onNegativeClick = onNegativeClick,
+                        onPositiveClick = onPositiveClick
+                    )
+                }
+            )
+        }
+        BottomSheet.Gender -> {
+            BottomSheet(
+                onDismiss = { showBottomSheet = BottomSheet.Default },
+                onNegativeClick = { showBottomSheet = BottomSheet.Default },
+                onPositiveClick = {
+                    accountViewModel.updateGender(it.toString().toInt())
+                    accountViewModel.updateUserProfile()
+                    showBottomSheet = BottomSheet.Default
+                },
+                contentSheet = {
+                        onNegativeClick, onPositiveClick ->
+
+                    BottomSheetGenderContent(
+                        onNegativeClick = onNegativeClick,
+                        onPositiveClick = onPositiveClick
+                    )
+                }
+            )
+        }
+    }
+
     Scaffold(
         topBar = {
             MyBodyTopAppBar()
         }
     ) { paddingValues ->
-        MyBodyScreen(paddingValues = paddingValues)
+        MyBodyScreen(
+            paddingValues = paddingValues,
+            height = accountInfoUIState.height ?: 0,
+            onHeightButtonClick = { showBottomSheet = BottomSheet.Height },
+            currentWeight = accountInfoUIState.currentWeight ?: 0.0,
+            onCurrentWeightButtonClick = { showBottomSheet = BottomSheet.CurrentWeight },
+            targetWeight = accountInfoUIState.targetWeight ?: 0.0,
+            onTargetWeightButtonClick = { showBottomSheet = BottomSheet.TargetWeight },
+            age = accountInfoUIState.birthdayDate ?: 0L,
+            onAgeButtonClick = { openDialog = true },
+            gender = accountInfoUIState.gender ?: 0,
+            onGenderButtonClick = { showBottomSheet = BottomSheet.Gender }
+        )
     }
 }
 
@@ -45,4 +227,265 @@ private fun MyBodyTopAppBar() {
 
         }
     )
+}
+
+enum class BottomSheet {
+    Default, Height, CurrentWeight, TargetWeight, Gender
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun BottomSheet(modifier: Modifier = Modifier,
+                        onDismiss: () -> Unit = {},
+                        onNegativeClick: () -> Unit = {},
+                        onPositiveClick: (Any) -> Unit = {},
+                        contentSheet: @Composable (onNegativeClick: () -> Unit, onPositiveClick: (Any) -> Unit) -> Unit = { _, _ -> }
+) {
+
+    val bottomSheet = rememberModalBottomSheetState(skipPartiallyExpanded = true)
+    val coroutineScope = rememberCoroutineScope()
+
+    ModalBottomSheet(
+        modifier = modifier,
+        onDismissRequest = {
+            coroutineScope.launch {
+                onDismiss()
+                bottomSheet.hide()
+            }
+        },
+        sheetState = bottomSheet,
+        dragHandle = { BottomSheetDefaults.DragHandle() },
+        containerColor = MaterialTheme.colorScheme.background,
+        tonalElevation = 0.dp
+    ) {
+
+        contentSheet(onNegativeClick, onPositiveClick)
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun BottomSheetHeightContent(modifier: Modifier = Modifier,
+                                     onNegativeClick: () -> Unit = { },
+                                     onPositiveClick: (Int) -> Unit = { _ -> }) {
+
+    val focusManager = LocalFocusManager.current
+    val color = MaterialTheme.colorScheme.primary
+
+    var height by remember {
+        mutableIntStateOf(0)
+    }
+
+    Column(modifier = modifier
+        .padding(16.dp)
+        .systemBarsPadding(), verticalArrangement = Arrangement.spacedBy(16.dp)) {
+        Text(text = "Enter Height",
+            style = MaterialTheme.typography.headlineSmall,
+            textAlign = TextAlign.Center,
+            modifier = modifier.fillMaxWidth(),
+            color = MaterialTheme.colorScheme.primary,
+            fontWeight = FontWeight.Bold)
+
+        Divider()
+
+        OutlinedTextField(
+            value = "$height",
+            onValueChange = { height = it.toIntOrNull() ?: 0 },
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(vertical = 16.dp),
+            shape = RoundedCornerShape(20),
+            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done, keyboardType = KeyboardType.Number),
+            keyboardActions = KeyboardActions(onDone = { focusManager.clearFocus() }),
+            colors = androidx.compose.material.TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = color,
+                unfocusedBorderColor = color,
+                backgroundColor = MaterialTheme.colorScheme.primaryContainer,
+                textColor = MaterialTheme.colorScheme.primary
+            )
+        )
+
+        Row(modifier = modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalAlignment = Alignment.CenterVertically) {
+
+            OutlinedButton(onClick = onNegativeClick, modifier = modifier
+                .weight(1f)
+                .requiredHeight(50.dp)) {
+                Text(text = stringResource(id = R.string.cancel))
+            }
+
+            Button(onClick = { onPositiveClick(height) }, modifier = modifier
+                .weight(1f)
+                .requiredHeight(50.dp)) {
+                Text(text = "Ok")
+            }
+        }
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun BottomSheetCurrentWeightContent(modifier: Modifier = Modifier, onNegativeClick: () -> Unit = { }, onPositiveClick: (Double) -> Unit = { _ -> }) {
+
+    val focusManager = LocalFocusManager.current
+    val color = MaterialTheme.colorScheme.primary
+
+    var currentWeight by remember {
+        mutableDoubleStateOf(0.0)
+    }
+
+    Column(modifier = modifier
+        .padding(16.dp)
+        .systemBarsPadding(), verticalArrangement = Arrangement.spacedBy(16.dp)) {
+        Text(text = "Enter Height",
+            style = MaterialTheme.typography.headlineSmall,
+            textAlign = TextAlign.Center,
+            modifier = modifier.fillMaxWidth(),
+            color = MaterialTheme.colorScheme.primary,
+            fontWeight = FontWeight.Bold)
+
+        Divider()
+
+        OutlinedTextField(
+            value = "$currentWeight",
+            onValueChange = { currentWeight = it.toDoubleOrNull() ?: 0.0 },
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(vertical = 16.dp),
+            shape = RoundedCornerShape(20),
+            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done, keyboardType = KeyboardType.Number),
+            keyboardActions = KeyboardActions(onDone = { focusManager.clearFocus() }),
+            colors = androidx.compose.material.TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = color,
+                unfocusedBorderColor = color,
+                textColor = MaterialTheme.colorScheme.primary
+            )
+        )
+
+        Row(modifier = modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalAlignment = Alignment.CenterVertically) {
+
+            OutlinedButton(onClick = onNegativeClick, modifier = modifier
+                .weight(1f)
+                .requiredHeight(50.dp)) {
+                Text(text = stringResource(id = R.string.cancel))
+            }
+
+            Button(onClick = { onPositiveClick(currentWeight) }, modifier = modifier
+                .weight(1f)
+                .requiredHeight(50.dp)) {
+                Text(text = "Ok")
+            }
+        }
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun BottomSheetTargetWeightContent(modifier: Modifier = Modifier, onNegativeClick: () -> Unit = { }, onPositiveClick: (Double) -> Unit = { _ -> }) {
+
+    val focusManager = LocalFocusManager.current
+    val color = MaterialTheme.colorScheme.primary
+
+    var targetWeight by remember {
+        mutableDoubleStateOf(0.0)
+    }
+
+    Column(modifier = modifier
+        .padding(16.dp)
+        .systemBarsPadding(), verticalArrangement = Arrangement.spacedBy(16.dp)) {
+        Text(text = "Enter Height",
+            style = MaterialTheme.typography.headlineSmall,
+            textAlign = TextAlign.Center,
+            modifier = modifier.fillMaxWidth(),
+            color = MaterialTheme.colorScheme.primary,
+            fontWeight = FontWeight.Bold)
+
+        Divider()
+
+        OutlinedTextField(
+            value = "$targetWeight",
+            onValueChange = { targetWeight = it.toDoubleOrNull() ?: 0.0 },
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(vertical = 16.dp),
+            shape = RoundedCornerShape(20),
+            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done, keyboardType = KeyboardType.Number),
+            keyboardActions = KeyboardActions(onDone = { focusManager.clearFocus() }),
+            colors = androidx.compose.material.TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = color,
+                unfocusedBorderColor = color,
+                textColor = MaterialTheme.colorScheme.primary
+            )
+        )
+
+        Row(modifier = modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalAlignment = Alignment.CenterVertically) {
+
+            OutlinedButton(onClick = onNegativeClick, modifier = modifier
+                .weight(1f)
+                .requiredHeight(50.dp)) {
+                Text(text = stringResource(id = R.string.cancel))
+            }
+
+            Button(onClick = { onPositiveClick(targetWeight) }, modifier = modifier
+                .weight(1f)
+                .requiredHeight(50.dp)) {
+                Text(text = "Ok")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun BottomSheetGenderContent(modifier: Modifier = Modifier, onNegativeClick: () -> Unit = { }, onPositiveClick: (Int) -> Unit = {}) {
+    Column(modifier = modifier
+        .padding(16.dp)
+        .systemBarsPadding(), verticalArrangement = Arrangement.spacedBy(16.dp)) {
+        Text(text = "Select Gender",
+            style = MaterialTheme.typography.headlineSmall,
+            textAlign = TextAlign.Center,
+            modifier = modifier.fillMaxWidth(),
+            color = MaterialTheme.colorScheme.primary,
+            fontWeight = FontWeight.Bold)
+
+        androidx.compose.material3.Divider()
+
+        Column(modifier = modifier
+            .fillMaxWidth()
+            .padding(top = 8.dp), verticalArrangement = Arrangement.spacedBy(24.dp)) {
+
+            Text(text = "Man",
+                style = MaterialTheme.typography.bodyLarge,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                modifier = modifier
+                    .fillMaxWidth()
+                    .clickable(
+                        onClick = { onPositiveClick(0) },
+                        interactionSource = remember { MutableInteractionSource() },
+                        indication = null))
+
+            Text(text = "Woman",
+                style = MaterialTheme.typography.bodyLarge,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                modifier = modifier.fillMaxWidth()
+                    .clickable(
+                        onClick = { onPositiveClick(1) },
+                        interactionSource = remember { MutableInteractionSource() },
+                        indication = null)
+            )
+        }
+    }
 }
--- app/src/main/java/com/bitvolper/yogazzz/presentation/mybody/MyBodyScreen.kt
+++ app/src/main/java/com/bitvolper/yogazzz/presentation/mybody/MyBodyScreen.kt
@@ -1,7 +1,9 @@
 package com.bitvolper.yogazzz.presentation.mybody
 
 import android.content.res.Configuration
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -10,47 +12,152 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Edit
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabPosition
+import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.bitvolper.yogazzz.presentation.theme.YogaAppTheme
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.rememberPagerState
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalPagerApi::class)
 @Composable
-fun MyBodyScreen(modifier: Modifier = Modifier, paddingValues: PaddingValues = PaddingValues()) {
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .padding(
-                top = paddingValues.calculateTopPadding(),
-                start = 16.dp,
-                end = 16.dp,
-                bottom = 16.dp
-            ),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
-    ) {
+fun MyBodyScreen(modifier: Modifier = Modifier,
+                 paddingValues: PaddingValues = PaddingValues(),
+                 height: Int = 0,
+                 onHeightButtonClick: () -> Unit = { },
+                 currentWeight: Double = 0.0,
+                 onCurrentWeightButtonClick: () -> Unit = { },
+                 targetWeight: Double = 0.0,
+                 onTargetWeightButtonClick: () -> Unit = {  },
+                 age: Long = 0L,
+                 onAgeButtonClick: () -> Unit = { },
+                 gender: Int = 0,
+                 onGenderButtonClick: () -> Unit = { }) {
 
-        BodyPartCard(title = "Height", value = "185 cm")
-        BodyPartCard(title = "Weight", value = "76.00 kg")
-        BodyPartCard(title = "Target Weight", value = "82.00 kg")
-        BodyPartCard(title = "Age", value = "28")
-        BodyPartCard(title = "Gender", value = "Male")
 
+    val context = LocalContext.current
+    val coroutineScope = rememberCoroutineScope()
+    val pager = rememberPagerState()
+
+
+    val title = listOf<String>("Kg, Cm", "Lbs, Ft")
+
+    Column(modifier = modifier
+        .fillMaxSize()
+        .padding(
+            top = paddingValues.calculateTopPadding()
+        )) {
+        Column(modifier = modifier.weight(1f), verticalArrangement = Arrangement.spacedBy(32.dp)) {
+            TabRow(
+                modifier = modifier.padding(horizontal = 16.dp),
+                selectedTabIndex = pager.currentPage,
+                indicator = { tabPositions: List<TabPosition> -> Box {} },
+                divider = { }) {
+                title.forEachIndexed { index, horizontalPagerContent ->
+                    Tab(
+                        selected = pager.currentPage == index,
+                        onClick = {
+                            coroutineScope.launch {
+                                pager.animateScrollToPage(index)
+                            }
+                        },
+                        text = {
+                            Text(
+                                text = title[index],
+                                color = if (pager.currentPage == index) MaterialTheme.colorScheme.onPrimary else MaterialTheme.colorScheme.onBackground
+                            )
+                        },
+                        modifier = if (pager.currentPage == index)
+                            Modifier
+                                .shadow(
+                                    elevation = 10.dp,
+                                    spotColor = Color.Transparent,
+                                    ambientColor = Color.Transparent
+                                )
+                                .background(
+                                    MaterialTheme.colorScheme.primary,
+                                    shape = RoundedCornerShape(
+                                        topStartPercent = if (index == 0) 20 else 0,
+                                        bottomStartPercent = if (index == 0) 20 else 0,
+                                        topEndPercent = if (index == 0) 0 else 20,
+                                        bottomEndPercent = if (index == 0) 0 else 20,
+                                    )
+                                )
+                        else Modifier.background(
+                            MaterialTheme.colorScheme.surfaceVariant,
+                            shape = RoundedCornerShape(
+                                topStartPercent = if (index == 0) 20 else 0,
+                                bottomStartPercent = if (index == 0) 20 else 0,
+                                topEndPercent = if (index == 0) 0 else 20,
+                                bottomEndPercent = if (index == 0) 0 else 20
+                            )
+                        )
+                    )
+                }
+            }
+
+            HorizontalPager(
+                count = title.size,
+                state = pager,
+                modifier = modifier
+                    .fillMaxWidth()
+            ) { index ->
+                when (index) {
+                    0 -> {
+                        Column(modifier = modifier.fillMaxSize().padding(horizontal = 16.dp),
+                            verticalArrangement = Arrangement.spacedBy(16.dp)
+                        ) {
+
+                            BodyPartCard(title = "Height", value = "$height cm", onClick = onHeightButtonClick)
+                            BodyPartCard(title = "Weight", value = "$currentWeight kg", onClick = onCurrentWeightButtonClick)
+                            BodyPartCard(title = "Target Weight", value = "$targetWeight kg", onClick = onTargetWeightButtonClick)
+                            BodyPartCard(title = "Age", value = "$age", onClick = onAgeButtonClick)
+                            BodyPartCard(title = "Gender", value = if (gender == 0) "Man" else "Woman", onClick = onGenderButtonClick)
+
+                        }
+                    }
+
+                    1 -> {
+                        Column(modifier = modifier.fillMaxSize().padding(horizontal = 16.dp),
+                            verticalArrangement = Arrangement.spacedBy(16.dp)
+                        ) {
+
+                            BodyPartCard(title = "Height", value = "$height cm")
+                            BodyPartCard(title = "Weight", value = "$currentWeight kg")
+                            BodyPartCard(title = "Target Weight", value = "$targetWeight kg")
+                            BodyPartCard(title = "Age", value = "$age", onClick = onAgeButtonClick)
+                            BodyPartCard(title = "Gender", value = if (gender == 0) "Man" else "Woman", onClick = onGenderButtonClick)
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-private fun BodyPartCard(modifier: Modifier = Modifier, title: String = "Height", value: String = "185cm") {
+private fun BodyPartCard(modifier: Modifier = Modifier, title: String = "Height", value: String = "185cm", onClick: () -> Unit = { }) {
     Card {
         Row(modifier = modifier
             .fillMaxWidth()
@@ -61,7 +168,7 @@ private fun BodyPartCard(modifier: Modifier = Modifier, title: String = "Height"
 
             Text(text = value, style = MaterialTheme.typography.bodyLarge, fontWeight = FontWeight.SemiBold)
 
-            IconButton(onClick = { /*TODO*/ }) {
+            IconButton(onClick = onClick) {
                 Icon(imageVector = Icons.Rounded.Edit, contentDescription = null, tint = MaterialTheme.colorScheme.primary)
             }
         }
